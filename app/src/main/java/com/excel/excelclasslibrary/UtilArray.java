b59aba63953cf92d5cd7e8b8214cd3342999c87a
= <=>
2016-09-06 05:31:18 +0530

    Fixed the bugs in getIPV4address() function and added new method getConnectedInterfaceName() method

--- app/src/main/java/com/excel/excelclasslibrary/UtilArray.java
+++ app/src/main/java/com/excel/excelclasslibrary/UtilArray.java
@@ -2,4 +2,55 @@ package com.excel.excelclasslibrary;
 
 public class UtilArray {
 
+    public static String[] sortLexicographically( String arr[] ){
+        String temp;
+        for( int i = 0 ; i < arr.length ; i++ ){
+            for( int j = i + 1 ; j < arr.length; j++ ){
+                if( arr[ j ].compareTo( arr[ i ]) < 0 ){
+                    temp = arr[ i ];
+                    arr[ i ] = arr[ j ];
+                    arr[ j ] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static String[] removeDuplicates( String arr[] ){
+        String temp[] = new String[ arr.length ];
+        arr = sortLexicographically( arr );
+        int counter = 0;
+        for( int i=0; i<arr.length ; i++ ){
+            for( int j = i + 1; j<arr.length ; j++ ){
+                if( arr[ j ].charAt( 0 ) == arr[ i ].charAt( 0 ) ){
+                    // IF first characters of current and next is same, then check if the strings are same
+                    if( ! arr[ j ].equals( arr[ i ] ) ){
+                        temp[ counter ] = arr[ i ];
+                        counter++;
+                        i = j;
+                        break;
+                    }
+                }
+                else{
+                    temp[ counter ] = arr[ i ];
+                    counter++;
+                    i = j;
+                    break;
+                }
+
+            }
+        }
+
+        // Count the number of values in temp excluding null
+        int i = 0;
+        for( i = 0; temp[ i ] != null ; i++ ){
+
+        }
+        String[] removed_duplicates = new String[ i ];
+        for( i = 0; i < removed_duplicates.length ; i++ ){
+            removed_duplicates[ i ] = temp[ i ];
+        }
+
+        return removed_duplicates;
+    }
 }
--- app/src/main/java/com/excel/excelclasslibrary/UtilMisc.java
+++ app/src/main/java/com/excel/excelclasslibrary/UtilMisc.java
@@ -3,17 +3,20 @@ package com.excel.excelclasslibrary;
 import android.content.Context;
 import android.content.Intent;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class UtilMisc {
 	
 	/**
 	 * 
-	 * @param Start an application using its package name
-	 * @param Context application context
-	 * @param String The name of the package  
+	 * Start an application using its package name
+	 * @param context application context
+	 * @param package_name The name of the package
 	 * @return true if no exception is raised, false otherwise with stack trace of the exception occurred in the Logs
 	 * 
 	 */
-	
 	public static boolean startApplicationUsingPackageName( Context context, String package_name ){
 		try{
 			Intent app_intent = context.getPackageManager().getLaunchIntentForPackage( package_name );
@@ -25,5 +28,31 @@ public class UtilMisc {
 		}
 		return false;
 	}
-	
+
+	/**
+	 *
+	 * Generate MD5 Hash of String
+	 * @param text The String whose md5 needs to be generated
+	 * @return md5 of the supplied string
+	 *
+	 */
+	public static String md5String( String text ){
+		MessageDigest m = null;
+		try {
+			m = MessageDigest.getInstance( "MD5" );
+		} catch ( NoSuchAlgorithmException e ) {
+			e.printStackTrace();
+			return null;
+		}
+		m.reset();
+		m.update( text.getBytes());
+		byte[] digest = m.digest();
+		BigInteger bigInt = new BigInteger( 1, digest );
+		String hashtext = bigInt.toString( 16 );
+		// Now we need to zero pad it if you actually want the full 32 chars.
+		while( hashtext.length() < 32 ){
+			hashtext = "0" + hashtext;
+		}
+		return hashtext;
+	}
 }
